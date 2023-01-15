1fc81a766063189fae1cefda17babf4d8bccaa4c
Floyd Everest <me@floydeverest.com>
2023-01-15 23:13:00 +1100

    bootstrapped repo

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    bg="gradient-to-br"
+    from="gray-700"
+    to="gray-900"
+    class="flex flex-col h-screen"
+  >
+    <Header />
+
+    <transition name="fade" mode="out-in">
+      <Nuxt />
+    </transition>
+
+    <Footer />
+  </div>
+</template>
+
+<script lang="ts">
+import Header from '../components/Header.vue';
+import Footer from '../components/Footer.vue';
+<script>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    bg="transparent"
+    h="screen"
+    class="grid place-items-center"
+  >
+    <p
+      font="extrabold"
+      text="transparent 8xl"
+      bg="clip-text gradient-to-r"
+      from="fuchsia-300"
+      via="violet-300"
+      to="rose-200"
+      justify="center"
+      class="flex"
+    >Under Construction</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
